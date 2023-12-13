5c0cbe9c7fe14db2740a0c5e909b55e33a56b85d
As7riX <73664193+As7riX@users.noreply.github.com>
2023-12-13 14:30:49 +0100

    Aggiunto codice nicola

--- src/DiscoCifrante/Main.java
+++ src/DiscoCifrante/Main.java
@@ -1,7 +1,33 @@
 package DiscoCifrante;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int Menu_Input;
+
+        do {
+            System.out.println("Menu del Il disco cifrante di Leon Battista Alberti ");
+            System.out.println();
+            System.out.println( "1) Inserisci il Messaggio(Senza caratteri accentati e H, K J, U, W, Y e Numeri ");
+
+            System.out.println("0) Esci");
+
+            Scanner sc = new Scanner(System.in);
+            Menu_Input = sc.nextInt();
+
+            switch(Menu_Input) {
+
+
+
+
+            }
+
+
+        }while(Menu_Input!=0);
+
+
     }
+
 }
