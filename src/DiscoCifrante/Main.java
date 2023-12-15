1e368481272565dc4906eeea439d0c4781f8251a
sassirosa <153761715+sassirosa@users.noreply.github.com>
2023-12-15 12:33:12 +0100

    Update Main.java

--- src/DiscoCifrante/Main.java
+++ src/DiscoCifrante/Main.java
@@ -10,16 +10,33 @@ public class Main {
         do {
             System.out.println("Menu del Il disco cifrante di Leon Battista Alberti ");
             System.out.println();
-            System.out.println( "1) Inserisci il Messaggio(Senza caratteri accentati e H, K J, U, W, Y e Numeri ");
-
+            System.out.println("1) Inserisci il Messaggio tutto in MAIUSCOLO PLS (senza i caratteri accentati e H, K J, U, W, Y e Numeri ");
+            System.out.println("2) ottieni messaggio criptato");
+            System.out.println("3) decripta messaggio criptato");
             System.out.println("0) Esci");
 
             Scanner sc = new Scanner(System.in);
             Menu_Input = sc.nextInt();
-
+            
+            string msg;
+            string cript;
+            char key;
+            
             switch(Menu_Input) {
-
-
+            
+            case 1: 
+            	msg = sc.nextString();
+            	break;
+            case 2:
+            	break;
+            case 3:
+            	System.out.println("inserisci messaggio criptato:");
+            	cript = sc.nextString();
+            	System.out.println("inserisci chiave di decriptazione:");
+            	key = sc.nextString();
+            	break;
+            default:
+            		break;
 
 
             }
