c7c137a55b67ccfcb93b2fa912415bdaed5f2d68
sassirosa <153761715+sassirosa@users.noreply.github.com>
2023-12-19 17:00:10 +0100

    Update Main.java

--- src/DiscoCifrante/Main.java
+++ src/DiscoCifrante/Main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+    	
+    	Criptazione c = new Criptazione();
+    	Decriptazioone d = Decriptazione();
+    	
         int Menu_Input;
 
         do {
@@ -13,28 +17,38 @@ public class Main {
             System.out.println("1) Inserisci il Messaggio tutto in MAIUSCOLO PLS (senza i caratteri accentati e H, K J, U, W, Y e Numeri ");
             System.out.println("2) ottieni messaggio criptato");
             System.out.println("3) decripta messaggio criptato");
-            System.out.println("0) Esci");
+            System.out.println("0) Esci :(");
 
             Scanner sc = new Scanner(System.in);
             Menu_Input = sc.nextInt();
             
-            String msg;
-            String cript;
-            char key;
+            String msg;		//messaggio utente in input
+            String cript;	// messaggio criptato in input
+            //char key;		// chiave di lettura in input
+            String output;	// messaggio criptato in output
+            String original;// messaggio decriptato in output
+            
             
             switch(Menu_Input) {
             
             case 1: 
             	msg = sc.nextLine();
             	break;
+            	
             case 2:
+            	output = c.cripta(msg);
+            	System.out.println("Messaggio criptato: " + output);
             	break;
+            	
             case 3:
             	System.out.println("inserisci messaggio criptato:");
             	cript = sc.nextLine();
-            	System.out.println("inserisci chiave di decriptazione:");
-            	key = sc.nextLine();
+            	//System.out.println("inserisci chiave di decriptazione:");
+            	//key = sc.next().charAt(0);
+            	original = d.decripta(cript);
+            	System.out.println("Messaggio decriptato: " + original);
             	break;
+            	
             default:
             		break;
 
@@ -48,3 +62,4 @@ public class Main {
     }
 
 }
+
