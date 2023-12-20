0505cabdcedcfa0ca4755616b9c648bc54061c96
HuNicolaVicentino <153913010+HuNicolaVicentino@users.noreply.github.com>
2023-12-20 21:46:03 +0100

    Update Criptazione.java
    mo funziona ma non inserisce i numeri , sto sistemando ma per ora vi mando questo che non si sa mai

--- src/DiscoCifrante/Criptazione.java
+++ src/DiscoCifrante/Criptazione.java
@@ -1,84 +1,67 @@
 package DiscoCifrante;
 
-import java.util.Random;
-
 public class Criptazione {
-	
-	 DiscoCifrante tabella;  // Roba del Garibaldi 
-	 private char chiave;  //Chiave per la Criptazione 
-	 
-	 
-	 public String Cripta (String Input)  {
-		  chiave=DiscoCifrante.getRandKey();
-		 tabella = new DiscoCifrante(chiave);
-		 int j=0;    //contatore di quanti elementi dello string ho inserito fino ad ora
+    DiscoCifrante tabella;  // Roba del Garibaldi
+    private char chiave;  // Chiave per la Criptazione
 
-	      Random rand = new Random();          //serve a generare un numero randomico per la dimensione sell'array di char
-	      int Dimensione = rand.nextInt(100);       //0-99 
-	      int size = Input.length();                //manda in Output La lunghezza dello string 
-		  char[] arrayChar = new char[size+Dimensione];   //creo un array di char di dimensione string + numero randomico
-		 
-		 
-		 
-	      arrayChar[0]=chiave;
-	    	//Questo for serve per Criptare numeri e elementi dello string
-		  //il i scorre per l'array di char creato e alla fine lo converte in string e lo manda in output
-		  //@ i= indice dell'array di char
-		  
-		 for(int i=1;i<(size + Dimensione);i++) {  
-			 int randomic;  //per contenere i numeri randomici
+    public String Cripta(String Input) {
+        chiave = DiscoCifrante.getRandKey();
+        tabella = new DiscoCifrante(chiave);
+        int j = 0;    // contatore di quanti elementi dello string ho inserito fino ad ora
 
-			 
-			 if(j<size) {  //Questo for serve a decidere se mettere un numero o elemento della cifra 
-				  int pesoNumero1 = (i*(100/size + Dimensione)); 
-			      int pesoNumero2 = ((size + Dimensione)-pesoNumero1);
-			      int numeroCasuale;
-			      
-				 if (rand.nextInt(pesoNumero1+pesoNumero2) < pesoNumero1) {
-			            numeroCasuale = 1; // Genera il primo numero con probabilità pesoNumero1
-			            
-			        } else {
-			            numeroCasuale = 0; // Genera il secondo numero con probabilità pesoNumero2
-			        }
-				 
-				 if(numeroCasuale==0) {   //Qui inserisco i numeri casuali da 1-4, li trasformo in char, li cripto e lo inserisco nel array di char					 
-					 do {
-							randomic = rand.nextInt(5);
-					 }while((randomic>=1)||(randomic<=4));
-					 
-					     char c = (char) randomic;
+        Random rand = new Random();          // serve a generare un numero randomico per la dimensione sell'array di char
+        int Dimensione = rand.nextInt(100);       // 0-99
+        int size = Input.length();                // manda in Output La lunghezza dello string
+        char[] arrayChar = new char[size + Dimensione];   // creo un array di char di dimensione string + numero randomico
 
-						arrayChar[i]=tabella.getCript(c);
-						tabella.setIndex(arrayChar[i]);
+        int randomic;  // per contenere i numeri randomici
+        char c; // dichiara c all'inizio del metodo
 
-				 }else {  //prende un elemento di Input con charat con indice j, lo cripta e poi lo inserische nell'array e incrementa il contatore
-					 
-					   char c = Input.charAt(j);
-						arrayChar[i]=tabella.getCript(c);
-						j++;
-					 
-				 }
-				 
-			 }else {
-				 
-				 do {    //stessa cosa di prima se era nel caso 0
-						randomic = rand.nextInt(5);
-				 }while((randomic>=1)||(randomic<=4));
-				 
-				    char c = (char) randomic;
+        arrayChar[0] = chiave;
+        // Questo for serve per Criptare numeri e elementi dello string
+        // il i scorre per l'array di char creato e alla fine lo converte in string e lo manda in output
+        // @ i= indice dell'array di char
+        int somma = size + Dimensione;
 
-					arrayChar[i]=tabella.getCript(c);
-					tabella.setIndex(arrayChar[i]);
-			 }
+        // Numero di volte che numeroCasuale deve essere settato a 1
+        int volteSettatoA1 = size;
 
-		 }
-		 
-	        String str = new String(arrayChar);  //trasforma l'array di char in string
-		 return str;
-		 
-	 }
+        for (int i = 1; i < somma; i++) {
+            int numeroCasuale;
 
-	
+            if (volteSettatoA1 > 0) {
+                numeroCasuale = 1;
+                volteSettatoA1--;
+            } else {
+                numeroCasuale = 0;
+            }
+
+            if (numeroCasuale == 0) {   // Qui inserisco i numeri casuali da 1-4, li trasformo in char, li cripto e lo inserisco nel array di char
+                do {
+                    randomic = rand.nextInt(5);
+                } while ((randomic >= 1) && (randomic <= 4));
+
+                c = (char) randomic;
+
+                arrayChar[i] = tabella.getCript(c);
+                tabella.setIndex(arrayChar[i]);
+
+            } else {  // prende un elemento di Input con charat con indice j, lo cripta e poi lo inserisce nell'array e incrementa il contatore
+                c = Input.charAt(j);
+                arrayChar[i] = tabella.getCript(c);
+                j++;
+            }
+
+        }
+
+        String str = new String(arrayChar);
+
+        System.out.println(str);
+
+        return str;
+    }
+}
+
 	 
 	
 	
