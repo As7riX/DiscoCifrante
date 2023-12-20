81dd5a9b557478048e44fea68a5aeea72f783512
As7riX <73664193+As7riX@users.noreply.github.com>
2023-12-20 12:19:11 +0100

    Sistemato Nicola

--- src/DiscoCifrante/Criptazione.java
+++ src/DiscoCifrante/Criptazione.java
@@ -4,33 +4,33 @@ import java.util.Random;
 
 public class Criptazione {
 	
-	 DiscoCifrante tabella;  // Roba del Garibaldi 
-	 private char chiave;  //Chiave per la Criptazione 
+	 DiscoCifrante tabella;  // Roba del Garibaldi
+	 private char chiave;  //Chiave per la Criptazione
 	 
 	 public Criptazione () {
 		 
 	 }
 	 
-	 public String Cripta (String Input)  {
+	 public String cripta(String Input)  {
 		 int j=0;    //contatore di quanti elementi dello string ho inserito fino ad ora
 
 	      Random rand = new Random();          //serve a generare un numero randomico per la dimensione sell'array di char
 	      int Dimensione = rand.nextInt(100);       //0-99 
-	      int size = Input.length();                //manda in Output La lunghezza dello string 
+	      int size = Input.length();                //manda in Output La lunghezza dello string
 		  char[] arrayChar = new char[size+Dimensione];   //creo un array di char di dimensione string + numero randomico
 		 
 		 
 		 
-	 
+
 	    	//Questo for serve per Criptare numeri e elementi dello string
 		  //il i scorre per l'array di char creato e alla fine lo converte in string e lo manda in output
 		  //@ i= indice dell'array di char
-		  
-		 for(int i=1;i<(size + Dimensione);i++) {  
+
+		 for(int i=1;i<(size + Dimensione);i++) {
 			 int randomic;  //per contenere i numeri randomici
 
 			 
-			 if(j<size) {  //Questo for serve a decidere se mettere un numero o elemento della cifra 
+			 if(j<size) {  //Questo for serve a decidere se mettere un numero o elemento della cifra
 				  int pesoNumero1 = (i*(100/size + Dimensione)); 
 			      int pesoNumero2 = ((size + Dimensione)-pesoNumero1);
 			      int numeroCasuale;
@@ -42,7 +42,7 @@ public class Criptazione {
 			            numeroCasuale = 0; // Genera il secondo numero con probabilità pesoNumero2
 			        }
 				 
-				 if(numeroCasuale==0) {   //Qui inserisco i numeri casuali da 1-4, li trasformo in char, li cripto e lo inserisco nel array di char					 
+				 if(numeroCasuale==0) {   //Qui inserisco i numeri casuali da 1-4, li trasformo in char, li cripto e lo inserisco nel array di char
 					 do {
 							randomic = rand.nextInt(5);
 					 }while((randomic>=1)||(randomic<=4));
