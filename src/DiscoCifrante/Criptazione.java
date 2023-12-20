da429dd13c807e8a37e7856bb12f5345d359bd52
HuNicolaVicentino <153913010+HuNicolaVicentino@users.noreply.github.com>
2023-12-20 11:40:46 +0100

    Update Criptazione.java
    ho risolto quasi tutti gli errori tranne 2 e altro

--- src/DiscoCifrante/Criptazione.java
+++ src/DiscoCifrante/Criptazione.java
@@ -4,27 +4,34 @@ import java.util.Random;
 
 public class Criptazione {
 	
-	 Cifrazione tabella;  //Cifrazione
+	 DiscoCifrante tabella;  //Cifrazione
 	 private char chiave='k';
 	 
 	 public Criptazione () {
 		 
 	 }
 	 
-	 public string Cripta (string Input)  {
+	 public String Cripta (String Input)  {
 	 	int Piazzamento;
 		 int j=0;
 
 	      Random rand = new Random();          //serve a generare un numero randomico per la dimensione sell'array di char
 	      int Dimensione = rand.nextInt(100);       //0-99 
-		 int size = length(Input);              //Vado a analizzare la dimnsione dello string in entrata
+	      int size = Input.length();
 		 char[] arrayChar = new char[size+Dimensione];   //creo un array di char di dimensione string + numero randomico
 		 
+		 
+		    char randomizedCharacter = (char) (rand.nextInt(26) + 'A');
+		    arrayChar[0]=randomizedCharacter;
+			 tabella.setIndex(arrayChar[0]);
 
-		 for(int i=0;i<(size + Dimensione);i++) {  //Tutto serve a Piazzare i vari char sparsi in ordine nel array 
+		 
+
+		 for(int i=1;i<(size + Dimensione);i++) {  //Tutto serve a Piazzare i vari char sparsi in ordine nel array 
+			 int randomic;
+
 			 
 			 if(j<size) {
-				 int random;
 				  int pesoNumero1 = (i*(100/size + Dimensione)); 
 			      int pesoNumero2 = ((size + Dimensione)-pesoNumero1);
 			      int numeroCasuale;
@@ -39,14 +46,15 @@ public class Criptazione {
 				 if(numeroCasuale==0) {
 					 
 					 do {
-							random = rand.nextInt(5);
-					 }while((random>=1)||(random<=4));
+							randomic = rand.nextInt(5);
+					 }while((randomic>=1)||(randomic<=4));
 					 
-						arrayChar[i]=//ROBA DEL GARIBALDI;
+						arrayChar[i]=tabella.getCript(randomic);
+						tabella.setIndex(arrayChar[i]);
 
 				 }else {
 					 
-						arrayChar[i]=// ROBA DEL GARIBALDI;
+						arrayChar[i]=tabella.getCript(chiave);
 						j++;
 					 
 				 }
@@ -54,11 +62,11 @@ public class Criptazione {
 			 }else {
 				 
 				 do {
-						random = rand.nextInt(5);
-				 }while((random>=1)||(random<=4));
+						randomic = rand.nextInt(5);
+				 }while((randomic>=1)||(randomic<=4));
 				 
-					arrayChar[i]=//ROBA DEL GARIBALDI
-
+					arrayChar[i]=tabella.getCript(randomic);
+					tabella.setIndex(arrayChar[i]);
 			 }
 
 		 }
