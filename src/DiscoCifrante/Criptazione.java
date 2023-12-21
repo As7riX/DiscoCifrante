6fb2867752b9ee90b3cfd23a75656706a87516e4
HuNicolaVicentino <153913010+HuNicolaVicentino@users.noreply.github.com>
2023-12-21 20:39:25 +0100

    Update Criptazione.java
    mo dovrebbe funzionare

--- src/DiscoCifrante/Criptazione.java
+++ src/DiscoCifrante/Criptazione.java
@@ -3,56 +3,63 @@ package DiscoCifrante;
 import java.util.Random;
 
 public class Criptazione {
-    DiscoCifrante tabella;  // Roba del Garibaldi
-    private char chiave;  // Chiave per la Criptazione
+    private DiscoCifrante tabella;
+    private char chiave;
 
-    public String Cripta(String Input) {
+    public String Cripta(String input) {
         chiave = DiscoCifrante.getRandKey();
-        tabella = new DiscoCifrante(chiave);
-        int j = 0;    // contatore di quanti elementi dello string ho inserito fino ad ora
+        tabella = new DiscoCifrante();
+        tabella.DiscoCifrante(chiave);
+        int j = input.length();
+        int J=0;
 
-        Random rand = new Random();          // serve a generare un numero randomico per la dimensione sell'array di char
-        int dimensioneMassimaDisco = 4; // Modifica a seconda della tua logica
-        int Dimensione = rand.nextInt(dimensioneMassimaDisco) + 1; // 1-4
-        int size = Input.length();                // manda in Output La lunghezza dello string
-        char[] arrayChar = new char[size + Dimensione + 2];   // creo un array di char di dimensione string + numero randomico
+        Random rand = new Random();
+        int dimensione = rand.nextInt(5) + 1;
+        int lunghezza = input.length() + dimensione + 2;
+        char[] arrayChar = new char[lunghezza];
 
         arrayChar[0] = chiave;
+        System.out.println(arrayChar[0]);
+
         arrayChar[1] = tabella.getRandIndex();
+        System.out.println(arrayChar[1]);
+
         tabella.setIndex(arrayChar[1]);
 
-        // Numero di volte che numeroCasuale deve essere settato a 1
-        int volteSettatoA1 = Math.min(size, dimensioneMassimaDisco);
-
-        for (int i = 2; i < size + Dimensione + 2; i++) {
+        for (int i = 2; i < lunghezza; i++) {
             int numeroCasuale;
 
-            if (volteSettatoA1 > 0) {
+            int peso1 = rand.nextInt(101) * j;
+            int peso2 = rand.nextInt(101) * dimensione;
+
+            if (peso1 >= peso2) {
                 numeroCasuale = 1;
-                volteSettatoA1--;
             } else {
                 numeroCasuale = 0;
             }
 
             if (numeroCasuale == 0) {
-                // Qui inserisco i numeri casuali da 1-4, li trasformo in char, li cripto e lo inserisco nel array di char
                 int randomic = rand.nextInt(4) + 49;
                 char c = (char) randomic;
+                System.out.println(c);
 
                 arrayChar[i] = tabella.getCript(c);
                 tabella.setIndex(arrayChar[i]);
             } else {
-                // Prende un elemento di Input con charat con indice j, lo cripta e poi lo inserisce nell'array e incrementa il contatore
-                char c = Input.charAt(j);
+                char c = input.charAt(J);
+                System.out.println(c);
+                J++;
 
                 arrayChar[i] = tabella.getCript(c);
-                j++;
+                j--;
             }
+
+            System.out.println(arrayChar[i]);
+            System.out.println();
         }
 
         String str = new String(arrayChar);
-
-        System.out.println(str);  // Puoi rimuovere questa stampa una volta completato il debug
+        System.out.println(str);
 
         return str;
     }
